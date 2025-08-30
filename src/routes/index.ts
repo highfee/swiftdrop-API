9aac677fb486979ba6ceea36870e52ee7fca6b6a
Kendrick9470 <odediranifeoluwa7@gmail.com>
2025-08-30 17:36:01 +0100

    CORS issue fixed

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes.js";
 import userRoutes from "./user.routes.js";
-import orderRoutes from "./order.routes.js";
+// import orderRoutes from "./order.routes.js";
 
 const router: Router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
-router.use("/orders", orderRoutes);
+// router.use("/orders", orderRoutes);
 
 export default router;
